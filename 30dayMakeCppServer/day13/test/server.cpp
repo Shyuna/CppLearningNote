ee272fa929598332fca3b5c795d36d22491aacf7
Shyuna <1229324428@qq.com>
2025-12-04 15:39:55 +0800

    change gitignore

---
+++ 30dayMakeCppServer/day13/test/server.cpp
@@ -0,0 +1,12 @@
+#include "EventLoop.h"
+#include "Server.h"
+
+int main()
+{
+    EventLoop *loop = new EventLoop();
+    Server *server = new Server(loop);
+    loop->loop();
+    delete loop;
+    delete server;
+    return 0;
+}
